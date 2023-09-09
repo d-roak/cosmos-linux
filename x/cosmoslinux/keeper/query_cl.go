c6ce95ef07fa323218fcbbbabcde6d83b801f9fe
d-roak <me+git@droak.sh>
2023-09-09 15:38:07 +0900

    add docker utils folder

--- x/cosmoslinux/keeper/query_cl.go
+++ x/cosmoslinux/keeper/query_cl.go
@@ -5,13 +5,11 @@ import (
 	"fmt"
 
 	"cosmos-linux/x/cosmoslinux/types"
+	"cosmos-linux/x/cosmoslinux/utils"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-
-	docker_types "github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
 )
 
 func (k Keeper) Cl(goCtx context.Context, req *types.QueryClRequest) (*types.QueryClResponse, error) {
@@ -24,20 +22,8 @@ func (k Keeper) Cl(goCtx context.Context, req *types.QueryClRequest) (*types.Que
 	// TODO: Process the query
 	_ = ctx
 
-    cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		panic(err)
-	}
-    defer cli.Close()
+    id, _ := utils.StartDockerContainer([]string{})
 
-	containers, err := cli.ContainerList(context.Background(), docker_types.ContainerListOptions{})
-	if err != nil {
-		panic(err)
-	}
-
-	for _, container := range containers {
-		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
-	}
-
-    return &types.QueryClResponse{Text: fmt.Sprintf("Hello %s", containers[0].Image)}, nil
+    return &types.QueryClResponse{Text: fmt.Sprintf("%s", id)}, nil
 }
+
---
+++ x/cosmoslinux/utils/docker.go
@@ -0,0 +1,86 @@
+package utils
+
+import (
+	"bufio"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+)
+
+func GetDockerContainer(id string, state string) (types.Container, error) {
+    cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+    if err != nil {
+        panic(err)
+    }
+    defer cli.Close()
+
+    containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+    if err != nil {
+        panic(err)
+    }
+
+    // TODO also validate state
+    for _, container := range containers {
+        if container.ID == id {
+            return container, nil
+        }
+    }
+
+    return types.Container{}, errors.New("Container not found")
+}
+
+func StartDockerContainer(commands []string) (string, error) {
+    ctx := context.Background()
+    cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+    if err != nil {
+        panic(err)
+    }
+    defer cli.Close()
+
+    reader, err := cli.ImagePull(ctx, "docker.io/library/alpine", types.ImagePullOptions{})
+    if err != nil {
+        panic(err)
+    }
+    io.Copy(os.Stdout, reader)
+
+    resp, err := cli.ContainerCreate(ctx, &container.Config{
+        Image: "alpine",
+        Cmd:   []string{"ls"},
+        Tty:   true,
+    }, nil, nil, nil, "")
+    if err != nil {
+        panic(err)
+    }
+
+    if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+        panic(err)
+    }
+
+    statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+    select {
+    case err := <-errCh:
+        if err != nil {
+            panic(err)
+        }
+    case <-statusCh:
+    }
+
+    out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+    if err != nil {
+        panic(err)
+    }
+
+    outString := ""
+    s := bufio.NewScanner(out)
+	for s.Scan() {
+        outString += fmt.Sprintf("%q\n", s.Bytes())
+	}
+
+    return outString, nil
+}
