cd70e228bec6fb3955ec07b44d2714000f9f0ade
sfsf9797 <sekfook97@gmail.com>
2023-09-09 17:09:59 +0900

    parse byte

--- x/cosmoslinux/utils/docker.go
+++ x/cosmoslinux/utils/docker.go
@@ -14,73 +14,73 @@ import (
 )
 
 func GetDockerContainer(id string, state string) (types.Container, error) {
-    cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-    if err != nil {
-        panic(err)
-    }
-    defer cli.Close()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	defer cli.Close()
 
-    containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
-    if err != nil {
-        panic(err)
-    }
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
 
-    // TODO also validate state
-    for _, container := range containers {
-        if container.ID == id {
-            return container, nil
-        }
-    }
+	// TODO also validate state
+	for _, container := range containers {
+		if container.ID == id {
+			return container, nil
+		}
+	}
 
-    return types.Container{}, errors.New("Container not found")
+	return types.Container{}, errors.New("Container not found")
 }
 
 func StartDockerContainer(commands []string) (string, error) {
-    ctx := context.Background()
-    cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-    if err != nil {
-        panic(err)
-    }
-    defer cli.Close()
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	defer cli.Close()
 
-    reader, err := cli.ImagePull(ctx, "docker.io/library/alpine", types.ImagePullOptions{})
-    if err != nil {
-        panic(err)
-    }
-    io.Copy(os.Stdout, reader)
+	reader, err := cli.ImagePull(ctx, "docker.io/library/alpine", types.ImagePullOptions{})
+	if err != nil {
+		panic(err)
+	}
+	io.Copy(os.Stdout, reader)
 
-    resp, err := cli.ContainerCreate(ctx, &container.Config{
-        Image: "alpine",
-        Cmd:   []string{"ls"},
-        Tty:   true,
-    }, nil, nil, nil, "")
-    if err != nil {
-        panic(err)
-    }
-
-    if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-        panic(err)
-    }
-
-    statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
-    select {
-    case err := <-errCh:
-        if err != nil {
-            panic(err)
-        }
-    case <-statusCh:
-    }
-
-    out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
-    if err != nil {
-        panic(err)
-    }
-
-    outString := ""
-    s := bufio.NewScanner(out)
-	for s.Scan() {
-        outString += fmt.Sprintf("%q\n", s.Bytes())
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "alpine",
+		Cmd:   []string{"ls"},
+		Tty:   true,
+	}, nil, nil, nil, "")
+	if err != nil {
+		panic(err)
 	}
 
-    return outString, nil
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		panic(err)
+	}
+
+	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			panic(err)
+		}
+	case <-statusCh:
+	}
+
+	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+	if err != nil {
+		panic(err)
+	}
+
+	outString := ""
+	s := bufio.NewScanner(out)
+	for s.Scan() {
+		outString += fmt.Sprintf("%q\n", s.Text())
+	}
+
+	return outString, nil
 }
